43ae51056b994c41150e99c56cc1a186880c2a22
Royson Lobo <royrenlobo@gmail.com>
2022-01-01 22:33:49 +0530

    import cv2

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask,render_template,request
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
-from cv2 import *
+# from cv2 import *
+import cv2
 import os
 # from keras.models import load_model
 import numpy as np
