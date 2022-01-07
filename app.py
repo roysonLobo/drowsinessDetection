f4780bf38730b800be81e29036e38b2619990ec6
Royson Lobo <royrenlobo@gmail.com>
2022-01-07 21:25:35 +0530

    changed videocapture

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def prediction():
 
     model = load_model('models/cnnCat2.h5')
     path = os.getcwd()
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_COMPLEX_SMALL
     count=0
     score=0
